774d95c2e5d012dc12086f6ae3fc377d43bd0304
LucaVmu <lucavmu@outlook.de>
2026-04-09 17:04:00 +0200

    Improved the deployment script a bit more

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-npm run build
+if [ -z "${1%@*}" ] || [ -z "${1#*@}" ]; then
+    echo "Please specify a valid ssh connection! (user@host)" >&2
+    exit 1
+fi
+
+[ "$2" != "--skip-build" ] && [ "$2" != "-s" ] && npm run build
 
 ssh "$1" /bin/sh <<'EOF'
 set -e
